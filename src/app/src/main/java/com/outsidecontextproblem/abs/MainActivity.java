400df5aff1ddb727ec218158f2937f0261f09d65
Stevö <stevehuwjohn@gmail.com>
2022-10-03 19:42:55 +0100

    WIP

--- src/app/src/main/java/com/outsidecontextproblem/abs/MainActivity.java
+++ src/app/src/main/java/com/outsidecontextproblem/abs/MainActivity.java
@@ -3,12 +3,13 @@ package com.outsidecontextproblem.abs;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import com.outsidecontextproblem.abs.services.WiFiMonitor;
 
--- src/app/src/main/java/com/outsidecontextproblem/abs/services/WiFiMonitor.java
+++ src/app/src/main/java/com/outsidecontextproblem/abs/services/WiFiMonitor.java
@@ -1,22 +1,28 @@
 package com.outsidecontextproblem.abs.services;
 
-import static android.Manifest.permission_group.LOCATION;
-
-import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.wifi.SupplicantState;
+import android.content.pm.ServiceInfo;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
-import androidx.core.app.ServiceCompat;
+import androidx.core.app.NotificationCompat;
+
+import com.outsidecontextproblem.abs.R;
 
 public class WiFiMonitor extends Service {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
@@ -31,7 +37,6 @@ public class WiFiMonitor extends Service {
 
                     WifiManager wifiMgr = (WifiManager) getSystemService(Context.WIFI_SERVICE);
                     WifiInfo wifiInfo = wifiMgr.getConnectionInfo();
-                    SupplicantState supplicantState = wifiInfo.getSupplicantState();
                     String name = wifiInfo.getSSID();
 
                     Log.e("Service", name);
@@ -46,6 +51,23 @@ public class WiFiMonitor extends Service {
                 }
             }
         ).start();
+
+        final String CHANNEL_ID = "Foreground Service Id";
+
+        NotificationChannel notificationChannel = new NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_ID,
+                NotificationManager.IMPORTANCE_DEFAULT
+        );
+
+        getSystemService((NotificationManager.class)).createNotificationChannel(notificationChannel);
+
+        Notification.Builder notificationBuilder = new Notification.Builder(this, CHANNEL_ID)
+                .setContentText("Blah")
+                .setContentTitle("Blah")
+                .setSmallIcon(R.drawable.ic_launcher_background);
+
+        startForeground(1001, notificationBuilder.build());
 
         return super.onStartCommand(intent, flags, startId);
     }
