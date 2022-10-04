6a08e993795ec756e14b6748d67ab52974966459
Stevö <stevehuwjohn@gmail.com>
2022-10-04 12:59:41 +0100

    WIP

--- src/app/src/main/java/com/outsidecontextproblem/abs/services/WiFiMonitorBroadcastReceiver.java
+++ src/app/src/main/java/com/outsidecontextproblem/abs/services/WiFiMonitorBroadcastReceiver.java
@@ -1,5 +1,6 @@
 package com.outsidecontextproblem.abs.services;
 
+import android.app.ActivityManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -8,8 +9,23 @@ public class WiFiMonitorBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: Don't start if already running.
+        if (serviceIsRunning(context)) {
+            return;
+        }
+
         Intent serviceIntent = new Intent(context, WiFiMonitor.class);
         context.startForegroundService(serviceIntent);
+    }
+
+    private boolean serviceIsRunning(Context context) {
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+
+        for (ActivityManager.RunningServiceInfo service: activityManager.getRunningServices(Integer.MAX_VALUE)) {
+            if (WiFiMonitor.class.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
