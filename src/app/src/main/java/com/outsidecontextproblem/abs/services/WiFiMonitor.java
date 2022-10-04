43ad80828bb79c969976b2fc3fe60cb222e39316
Stevö <stevehuwjohn@gmail.com>
2022-10-04 11:03:02 +0100

    WIP

--- src/app/src/main/java/com/outsidecontextproblem/abs/services/WiFiMonitor.java
+++ src/app/src/main/java/com/outsidecontextproblem/abs/services/WiFiMonitor.java
@@ -6,16 +6,12 @@ import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ServiceInfo;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.IBinder;
 import android.util.Log;
-import android.widget.TextView;
 
 import androidx.annotation.Nullable;
-import androidx.core.app.NotificationCompat;
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.outsidecontextproblem.abs.R;
 
